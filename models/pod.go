1e2d216a81e87abb00af72fe553f4b1777f0d9c4
Karim <karim.wael@gmail.com>
2022-03-12 22:24:25 +0400

    clean slate for using redis pipes instead of sockets

--- models/pod.go
+++ models/pod.go
@@ -1,9 +1,6 @@
 package models
 
-import "github.com/karim-w/emolga/modules/scclient"
-
 type Pod struct {
 	PodName string `json:"id"`
 	PodIp   string `json:"ip"`
-	Client  scclient.Client
 }
--- services/podManager.go
+++ services/podManager.go
@@ -2,7 +2,6 @@ package services
 
 import (
 	"github.com/karim-w/emolga/models"
-	"github.com/karim-w/emolga/modules/scclient"
 	"go.uber.org/fx"
 	"go.uber.org/zap"
 )
@@ -13,31 +12,7 @@ type PodManager struct {
 }
 
 func (p *PodManager) AddPod(pod models.Pod) {
-	client := scclient.New("ws://" + pod.PodIp + "/socketcluster/")
-	/*
-			{
-		            username: "kyle",
-		            password: "thebestpassword",
-		            client_name: this.client_name,
-		          }
-	*/
-	loginJson := make(map[string]string, 3)
-	loginJson["username"] = "kyle"
-	loginJson["password"] = "thebestpassword"
-	loginJson["client_name"] = "emolga"
-	// client.SetBasicListener(p.onConnect, p.onConnectError, p.onDisconnect)
-	//client.SetAuthenticationListener(p.onSetAuthentication, p.onAuthentication)
-	go func() {
-		client.Connect()
-		client.Emit("login", loginJson)
-		//  func(eventName string, error interface{}, data interface{}) {
-		// 	if error == nil {
-		// 		// fmt.Println("Got ack for emit event with data ", data, " and error ", error)
-		// 		pod.Client = client
-		// 		p.pods = append(p.pods, pod)
-		// 	}
-		// })
-	}()
+	p.pods = append(p.pods, pod)
 }
 
 func (p *PodManager) RemovePod(podId string) {
@@ -47,28 +22,6 @@ func (p *PodManager) RemovePod(podId string) {
 			return
 		}
 	}
-}
-
-//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\::::: SocketClusterHandlers :::::::://\\//\\//\\//\\//\\//\\//\\//\\//\\//\\
-func (p *PodManager) onConnect(scc scclient.Client) {
-	p.logger.Info("Connected to socketcluster server")
-}
-
-func (p *PodManager) onDisconnect(scc scclient.Client, err error) {
-	p.logger.Info("Disconnected from socketcluster server")
-	p.logger.Error(err)
-}
-
-func (p *PodManager) onConnectError(scc scclient.Client, err error) {
-	p.logger.Error(err)
-}
-
-func (p *PodManager) onSetAuthentication(scc scclient.Client, token string) {
-	p.logger.Info("Client Received Auth token :	", token)
-}
-
-func (p *PodManager) onAuthentication(scc scclient.Client, isAuthenticated bool) {
-	p.logger.Info("Client Authentication Status :	", isAuthenticated)
 }
 
 //\\//\\//\\//\\//\\//\\//\\//\\//\\//\\::::: DI :::::::://\\//\\//\\//\\//\\//\\//\\//\\//\\//\\
